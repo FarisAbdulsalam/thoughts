221debf156a46be06d43e4e08c623a8f821e9167
Faris <farisalmannaei@outlook.com>
2025-09-19 17:33:15 +0300

    fixed issue in urls.py, made templates for blog_detail and blog_index

--- thoughts/main_app/urls.py
+++ thoughts/main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('blog/<int:pk>/', views.blog_detail, name='blog_detail'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
